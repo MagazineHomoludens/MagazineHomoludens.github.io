6e8adf531aff53e4519fbb13fc578de52c82596a
num22yh <yun1998ha@naver.com>
2022-12-13 05:02:28 +0900

    메모리즈

---
+++ sample.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $("button").click(function () {
+    $(".modal").fadeIn();
+  });
+
+  $(".modal_content").click(function () {
+    $(".modal").fadeOut();
+  });
+});
